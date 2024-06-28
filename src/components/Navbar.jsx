ec22aaac5d28bdf6a07ef020eae15e80cfd0582e
KazbundaYTB <100788815+KazbundaYTB@users.noreply.github.com>
2024-06-28 18:52:33 +0200

    Added logo to navbar.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { FiMenu, FiX } from "react-icons/fi"; // Importing the FiMenu and FiX icons from react-icons
-
+import logo from "../img/logo.png"
 export default function Navbar() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
@@ -31,8 +31,7 @@ export default function Navbar() {
   return (
     <div className="w-full h-[10%] bg-neutral-800 flex justify-between items-center">
       <div className="pl-4 ml-16">
-        <h1 className="text-white text-lg font-bold">Logo</h1>
-        <h1 className="text-white text-lg font-bold">{windowWidth}</h1>
+        <img src={logo} alt="" />
       </div>
       <div className="pr-4 mr-16">
         {windowWidth <= 850 ? (
@@ -53,23 +52,23 @@ export default function Navbar() {
           // Render regular nav links for larger screens
           <ul className="flex flex-row gap-5 text-white">
             <li>
-              <a href="http://">Menu</a>
+              <a href="#">Domov</a>
             </li>
             <li>
-              <a href="http://">Menu</a>
+              <a href="#">Minecraft</a>
             </li>
             <li>
-              <a href="http://">Menu</a>
+              <a href="#">Webhost</a>
             </li>
             <li>
-              <a href="http://">Menu</a>
+              <a href="#">Databáze</a>
             </li>
           </ul>
         )}
       </div>
       {/* Sidebar */}
       {windowWidth <= 850 && isSidebarOpen && (
-        <div className="fixed top-0 right-0 h-full w-64 bg-neutral-900 bg-opacity-45 z-50">
+        <div className="fixed top-0 right-0 h-full w-64 bg-neutral-900 bg-opacity-95 z-50">
           {/* Close button */}
           <div className="flex justify-end p-4">
             <FiX
@@ -80,16 +79,16 @@ export default function Navbar() {
           </div>
           {/* Menu items */}
           <ul className="py-4">
-            <li className="text-white text-lg py-2 px-4 hover:bg-neutral-900 bg-opacity-10 cursor-pointer">
+            <li className="text-white text-lg py-2 px-4 bg-opacity-10 cursor-pointer">
               <a href="http://">Menu Item 1</a>
             </li>
-            <li className="text-white text-lg py-2 px-4 hover:bg-neutral-900 cursor-pointer">
+            <li className="text-white text-lg py-2 px-4 cursor-pointer">
               <a href="http://">Menu Item 2</a>
             </li>
-            <li className="text-white text-lg py-2 px-4 hover:bg-neutral-900 cursor-pointer">
+            <li className="text-white text-lg py-2 px-4 cursor-pointer">
               <a href="http://">Menu Item 3</a>
             </li>
-            <li className="text-white text-lg py-2 px-4 hover:bg-neutral-900 cursor-pointer">
+            <li className="text-white text-lg py-2 px-4 cursor-pointer">
               <a href="http://">Menu Item 4</a>
             </li>
           </ul>
