475b420e1a39355e3b6f4a83fa0159500626e186
KazbundaYTB <100788815+KazbundaYTB@users.noreply.github.com>
2024-06-28 19:09:20 +0200

    Added Footer text.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div className='w-full h-[10%] bg-neutral-800'>
-
+    <div className='w-full h-[10%] bg-neutral-800 flex flex-col justify-center items-center'>
+        <h1 className=' text-white text-lg flex flex-row '> Made by RichardPavlik with ❤️</h1>
     </div>
   )
 }
