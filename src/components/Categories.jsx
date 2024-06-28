197def14bb45899c743e9f458ac848bc9bd83096
KazbundaYTB <100788815+KazbundaYTB@users.noreply.github.com>
2024-06-28 18:53:11 +0200

    Changed design of categories.

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -20,33 +20,49 @@ export default function Categories() {
       <div className="mt-12">
         <div className={`grid gap-4 ${windowWidth <= 890 ? 'grid-cols-1' : windowWidth <= 1680 ? 'grid-cols-2 ' : 'grid-cols-4'} justify-center items-center`}>
 
-        <div className="bg-gray-800 w-[400px] h-[320px] rounded-xl overflow-hidden">
-          <div className="bg-gray-700 w-full h-[70px] rounded-t-xl">
+          <div className="bg-gray-800 w-[400px] h-[320px] rounded-xl overflow-hidden">
+            <div className="bg-gray-700 w-full h-[70px] rounded-t-xl flex justify-center items-center">
+              <h1 className="font-bold text-xl text-white">Minecraft</h1>
+            </div>   
+
+            <div className="bg-gray-700 w-[90%] h-[220px] mt-3 mb-3 flex justify-center items-center mx-auto rounded-l ">
+              <h1 className="font-bold text-xl text-white">Posts</h1>
+            </div>
 
           </div>
 
-      </div>
-
-      <div className="bg-gray-800 w-[400px] h-[320px] rounded-xl overflow-hidden">
-          <div className="bg-gray-700 w-full h-[70px] rounded-t-xl">
+          <div className="bg-gray-800 w-[400px] h-[320px] rounded-xl overflow-hidden">
+            <div className="bg-gray-700 w-full h-[70px] rounded-t-xl flex justify-center items-center">
+              <h1 className="font-bold text-xl text-white">Webhost</h1>
+            </div>
+            
+            <div className="bg-gray-700 w-[90%] h-[220px] mt-3 mb-3 flex justify-center items-center mx-auto rounded-l ">
+              <h1 className="font-bold text-xl text-white">Posts</h1>
+            </div>
 
           </div>
 
-      </div>
+          <div className="bg-gray-800 w-[400px] h-[320px] rounded-xl overflow-hidden">
+            <div className="bg-gray-700 w-full h-[70px] rounded-t-xl flex justify-center items-center">
+              <h1 className="font-bold text-xl text-white">Databáze</h1>
+            </div>
 
-      <div className="bg-gray-800 w-[400px] h-[320px] rounded-xl overflow-hidden">
-        <div className="bg-gray-700 w-full h-[70px] rounded-t-xl">
+            <div className="bg-gray-700 w-[90%] h-[220px] mt-3 mb-3 flex justify-center items-center mx-auto rounded-l ">
+              <h1 className="font-bold text-xl text-white">Posts</h1>
+            </div>
 
-        </div>
-
-      </div>
+          </div>
       
-      <div className="bg-gray-800 w-[400px] h-[320px] rounded-xl overflow-hidden">
-        <div className="bg-gray-700 w-full h-[70px] rounded-t-xl">
+          <div className="bg-gray-800 w-[400px] h-[320px] rounded-xl overflow-hidden">
+            <div className="bg-gray-700 w-full h-[70px] rounded-t-xl flex justify-center items-center">
+              <h1 className="font-bold text-xl text-white">Již brzy</h1>
+            </div>
 
-        </div>
+            <div className="bg-gray-700 w-[90%] h-[220px] mt-3 mb-3 flex justify-center items-center mx-auto rounded-l ">
+              <h1 className="font-bold text-xl text-white">Posts</h1>
+            </div>
 
-      </div>
+          </div>
 
         </div>
       </div>
