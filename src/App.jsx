6bbf03c45c410c9986fd63cb4f6487dccf8184d2
KazbundaYTB <100788815+KazbundaYTB@users.noreply.github.com>
2024-06-28 18:29:31 +0200

    Added Title to homepage.

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Categories from './components/Categories';
 import Footer from "./components/Footer"
+import Title from './components/Title';
 
 export default function App() {
   return (
     <div className="w-screen h-screen overflow-x-hidden">
 
         <Navbar />
+        <Title />    
         <Categories />    
         <Footer />
       
---
+++ src/components/Title.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Title() {
+    return (
+        <div className="w-full h-[20%] bg-neutral-900 flex flex-col justify-center items-center">
+            <h1 className="text-white text-3xl font-bold">Znalostní databáze</h1>
+            <div className='w-[220px] h-[2px] bg-white'></div>
+            <h1 className="text-white text-xl mt-3">Nevíš si s něčím rady? Zkus se podívat sem, tady najdeš odpověď!</h1>
+
+        </div>
+      );
+}
