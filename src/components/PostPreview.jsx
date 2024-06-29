920cb679611c49b738fa97d3b49ee311e58cc153
KazbundaYTB <100788815+KazbundaYTB@users.noreply.github.com>
2024-06-29 20:44:10 +0200

    Changed the text display type in the post.

--- src/components/PostPreview.jsx
+++ src/components/PostPreview.jsx
@@ -1,20 +1,8 @@
 import React from 'react';
 
 export default function PostPreview({ post }) {
-  // Kontrola existence a typu post.content
-  if (!Array.isArray(post.content)) {
-    return (
-      <div className='w-screen h-screen flex flex-col justify-start items-center bg-neutral-900'>
-        <div className='w-screen mt-24 text-white flex flex-col justify-center items-center text-3xl font-bold'>
-          <h1>{post.title}</h1>
-          <div className='w-[120px] h-[1px] mt-2 bg-white'></div>
-        </div>
-        <div className='w-screen mt-24 text-white flex flex-col justify-start items-center text-3xl font-bold'>
-          <p>Obsah není k dispozici nebo není ve správném formátu.</p>
-        </div>
-      </div>
-    );
-  }
+  // Ensure post and post.content exist and are of the correct type
+  const contentArray = Array.isArray(post.content) ? post.content : [post.content];
 
   return (
     <div className='w-screen h-screen flex flex-col justify-start items-center bg-neutral-900'>
@@ -24,8 +12,8 @@ export default function PostPreview({ post }) {
       </div>
       <div className='w-[80%] mt-24 text-white flex flex-col justify-center text-3xl font-bold'>
         {/* Mapování pole post.content */}
-        {post.content.map((sentence, index) => (
-          <div key={index} className='text-white ml-48'>{sentence}</div>
+        {contentArray.map((sentence, index) => (
+          <h1 key={index} dangerouslySetInnerHTML={{ __html: sentence }}></h1>
         ))}
       </div>
     </div>
