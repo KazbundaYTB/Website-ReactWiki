beb6d5b32d7afe1950aac9f7f1f1cd3f7336bb46
KazbundaYTB <100788815+KazbundaYTB@users.noreply.github.com>
2024-06-29 22:11:30 +0200

    Changed PostPreview.

--- src/components/PostPreview.jsx
+++ src/components/PostPreview.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
-
+import Navbar from './Navbar';
+import Footer from './Footer';
 export default function PostPreview({ post }) {
   // Ensure post and post.content exist and are of the correct type
   const contentArray = Array.isArray(post.content) ? post.content : [post.content];
 
   return (
-    <div className='w-screen h-screen flex flex-col justify-start items-center bg-neutral-900'>
+    <div className='w-screen h-screen flex flex-col justify-start items-center bg-neutral-900 '>
+      <Navbar />
       <div className='w-screen mt-24 text-white flex flex-col justify-center items-center text-3xl font-bold'>
         <h1>{post.title}</h1>
         <div className='w-[120px] h-[1px] mt-2 bg-white'></div>
       </div>
-      <div className='w-[80%] mt-24 text-white flex flex-col justify-center text-3xl font-bold'>
+      <div className='w-[80%] mt-24 text-white flex flex-col justify-center text-xl font-bold'>
         {/* Mapování pole post.content */}
         {contentArray.map((sentence, index) => (
           <h1 key={index} dangerouslySetInnerHTML={{ __html: sentence }}></h1>
